ab6f61ccd847163db5f901c08e3d59c90e34266b
UjjwalBharti13 <112484492+UjjwalBharti13@users.noreply.github.com>
2025-01-07 00:20:28 +0530

    Create Who_is_the_winner.cpp

---
+++ Microsoft Q/Who_is_the_winner.cpp
@@ -0,0 +1,10 @@
+// probelm link - https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+               if (n==1) return 1;
+       return (findTheWinner(n-1, k)+(k-1))%n+1;
+
+    }
+};
